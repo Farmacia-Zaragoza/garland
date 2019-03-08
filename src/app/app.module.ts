611ab84b3da658d0b0d199b63f24239a92f6b7be
undefinedmamun <bkmmamun@gmail.com>
2019-03-08 16:59:05 +0600

    foundation done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopSectionComponent } from './top-section/top-section.component';
+import { LeftSectionComponent } from './left-section/left-section.component';
+import { RightSectionComponent } from './right-section/right-section.component';
+import { BottomSectionComponent } from './bottom-section/bottom-section.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopSectionComponent,
+    LeftSectionComponent,
+    RightSectionComponent,
+    BottomSectionComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
