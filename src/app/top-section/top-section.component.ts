b492df9da79c773bb55e620ae1edbac4f9db98f5
undefinedmamun <bkmmamun@gmail.com>
2019-03-08 23:00:56 +0600

    topSection done

--- src/app/top-section/top-section.component.ts
+++ src/app/top-section/top-section.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-top-section',
-  templateUrl: './top-section.component.html',
-  styleUrls: ['./top-section.component.scss']
+  selector: "app-top-section",
+  templateUrl: "./top-section.component.html",
+  styleUrls: ["./top-section.component.scss"]
 })
 export class TopSectionComponent implements OnInit {
+  styles = {
+    "background-color": "#000",
+    "background-image":
+      "url(http://cica.dbrqx.com/rimg/cica/images/brqx_hozdepriegotajoosa_0512x0192.png)"
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
