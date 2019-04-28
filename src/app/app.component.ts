8139ff82006c8e6609a7e902e80503bcf5ae32b7
undefinedmamun <bkmmamun@gmail.com>
2019-04-28 15:42:05 +0600

    initial responsive version done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,34 @@
 import { PageService } from "./page.service";
 import { Component, OnInit } from "@angular/core";
+import {
+  bounceInRightAnimation,
+  bounceInLeftAnimation,
+  bounceOutRightAnimation,
+  bounceOutLeftAnimation,
+  fadeInRightAnimation,
+  fadeOutRightAnimation,
+  fadeInLeftAnimation,
+  fadeOutLeftAnimation
+} from "angular-animations";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  styleUrls: ["./app.component.scss"],
+  animations: [
+    bounceInRightAnimation(),
+    bounceOutRightAnimation(),
+    bounceInLeftAnimation(),
+    bounceOutLeftAnimation(),
+
+    fadeInRightAnimation(),
+    fadeOutRightAnimation(),
+    fadeInLeftAnimation(),
+    fadeOutLeftAnimation()
+  ]
 })
 export class AppComponent implements OnInit {
   constructor(private service: PageService) {}
-
-  customOptions: any = {
-    loop: true,
-    mouseDrag: false,
-    touchDrag: false,
-    pullDrag: false,
-    dots: false,
-    navSpeed: 700,
-    navText: [
-      "<i class='fa fa-angle-left'></i>",
-      "<i class='fa fa-angle-right'></i>"
-    ],
-    responsive: {
-      0: {
-        items: 1
-      }
-    },
-    nav: true
-  };
 
   configs = [];
   leftStyle = {
@@ -35,34 +37,14 @@ export class AppComponent implements OnInit {
   };
   rightStyle = { value: "fff" };
 
-  menuItems = [
-    { link: "#", item: "item1" },
-    { link: "#", item: "item2" },
-    { link: "#", item: "item3" },
-    { link: "#", item: "item4" },
-    { link: "#", item: "item5" },
-    { link: "#", item: "item6" },
-    { link: "#", item: "item7" },
-    { link: "#", item: "item8" },
-    { link: "#", item: "item9" },
-    { link: "#", item: "item10" },
-    { link: "#", item: "item11" },
-    { link: "#", item: "item11" },
-    { link: "#", item: "item11" },
-    { link: "#", item: "item11" },
-    { link: "#", item: "item11" }
-  ];
+  leftVisible = false;
+  rightVisible = false;
 
   ngOnInit() {
     this.service.done.subscribe(data => {
       this.leftStyle = this.service.getLeft().styles;
 
       this.rightStyle = this.service.getRight().styles;
-
-      // console.log(this.leftStyle);
-      // this.configs = data["common_json"]["main"];
-      // console.log(this.rightConfig());
-      // console.log(this.mainItems);
     });
   }
 
