b3a9325d4ddf0bdfe87b030a299171d6cddbc62f
undefinedmamun <bkmmamun@gmail.com>
2020-01-26 20:32:20 +0600

    bread crumb initialized

--- src/app/bread-crumb/bread-crumb.component.ts
+++ src/app/bread-crumb/bread-crumb.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import {
+  slideInDownOnEnterAnimation,
+  slideOutUpOnLeaveAnimation
+} from "angular-animations";
 @Component({
-  selector: 'app-bread-crumb',
-  templateUrl: './bread-crumb.component.html',
-  styleUrls: ['./bread-crumb.component.scss']
+  selector: "app-bread-crumb",
+  templateUrl: "./bread-crumb.component.html",
+  styleUrls: ["./bread-crumb.component.scss"],
+  animations: [
+    slideInDownOnEnterAnimation({ duration: 500 }),
+    slideOutUpOnLeaveAnimation({ duration: 500 })
+  ]
 })
 export class BreadCrumbComponent implements OnInit {
+  constructor() {}
+  visible = false;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
